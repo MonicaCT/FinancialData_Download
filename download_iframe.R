465a8bebe097b83cfe0996d0562f36ef8abb4997
Songeo <sonia.gmcl@gmail.com>
2020-10-18 20:17:40 -0500

    new case iframe

---
+++ download_iframe.R
@@ -0,0 +1,20 @@
+
+# date: 2020-10
+# author: @songeo
+
+
+# iframes 
+#' Therefore, to extract content in an iframe, you need to find the link to the iframe HTML.
+
+
+# libraries
+library(tidyverse)
+library(here)
+library(glue)
+library(readxl)
+# web scrapping libraries
+library(httr)
+library(rvest)
+
+
+# 0. functions ----
