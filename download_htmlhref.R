c8a810927d96a6a95a2fdbdd1ac3137e3680838d
Songeo <sonia.gmcl@gmail.com>
2020-10-18 19:55:40 -0500

    datos de chile

--- download_htmlhref.R
+++ download_htmlhref.R
@@ -1,6 +1,6 @@
 
 
-# Colombia ----
+
 
 # libraries
 library(tidyverse)
@@ -20,18 +20,20 @@ library(rvest)
 #'
 #' @return a nice 'Done!' message
 download_xlsx <- 
-  function(xlsx_url, data_path = '.'){
+  function(xlsx_url, head_url, data_path = '.'){
     url_download <- 
-      glue("https://www.superfinanciera.gov.co{xlsx_url}")
+      glue("{head_url}{xlsx_url}")
+      # glue("https://www.superfinanciera.gov.co{xlsx_url}")
     
     message(glue("downloading {url_download}"))
     # No check certificate
-    system(glue('wget --no-check-certificate "{url_download}" -P {data_path}'))
+    system(glue('wget --no-check-certificate --user-agent="Mozilla" "{url_download}" -P {data_path}'))
     # system(glue('wget --no-check-certificate "{url_download}"'))
     
     return('Done!')
   }
 
+# Colombia ----
 # 1. main url to search ----
 main_url <- "https://www.superfinanciera.gov.co/inicio/informes-y-cifras/cifras/establecimientos-de-credito/informacion-periodica/mensual/evolucion-cartera-de-creditos-60950"
 
@@ -40,8 +42,7 @@ html_url <-
   main_url %>% 
   read_html()
 xlsx_urls <- 
-  html_nodes(main_url %>% 
-               read_html(), 
+  html_nodes(html_url, 
              css = "ul, a") %>% 
   html_attr('href') %>% 
   str_subset('.xlsx') %>% 
@@ -49,7 +50,7 @@ xlsx_urls <-
 xlsx_urls %>% length()
 
 
-# 3. iterative downloading file
+# 3. iterative downloading file ----
 exists_dir <- 
   list.dirs(path = here::here()) %>% 
   str_detect('data_colombia') %>% 
@@ -57,11 +58,12 @@ exists_dir <-
 if(!exists_dir){system('mkdir data_colombia')}
 map(xlsx_urls[1],
     download_xlsx,
+    head_url = "https://www.superfinanciera.gov.co",
     data_path = glue("{here::here()}/data_colombia"))
 
-# https://www.superfinanciera.gov.co/descargas/institucional/pubFile1048244/ca072020.xlsx
 
-# 4. reading .xlsx file
+
+# 4. reading .xlsx file ----
 xlsx_files <- 
   list.files("data_colombia", full.names = T)
 
@@ -71,4 +73,44 @@ list_download <-
       function(sh){
         read_xlsx(glue("{here::here()}/{file_path}"), sheet = sh)      
         })
+list_download
+
+
+# Chile ----
+# 1. main url to search ----
+main_url <- "http://www.cmfchile.cl/portal/estadisticas/606/w3-propertyvalue-29507.html"
+
+# 2. searching urls to download ----
+html_url <- 
+  main_url %>% 
+  read_html()
+xlsx_urls <- 
+  html_nodes(html_url, 
+             css = ".format-xlsx, a") %>% 
+  html_attr('href') %>% 
+  str_subset('.xlsx') 
+xlsx_urls %>% length()
+
+# 3. iterative downloading file ----
+exists_dir <- 
+  list.dirs(path = here::here()) %>% 
+  str_detect('data_chile') %>% 
+  sum %>% {.>0}
+if(!exists_dir){system('mkdir data_chile')}
+map(xlsx_urls[1],
+    download_xlsx,
+    head_url = "http://www.cmfchile.cl/portal/estadisticas/606/",
+    data_path = glue("{here::here()}/data_chile"))
+
+
+# 4. reading .xlsx file ----
+xlsx_files <- 
+  list.files("data_chile", full.names = T)
+
+file_path <- xlsx_files[1]
+list_download <- 
+  map(readxl::excel_sheets(path = glue("{here::here()}/{file_path}")), 
+      function(sh){
+        read_xlsx(glue("{here::here()}/{file_path}"), sheet = sh)      
+      })
 list_download
